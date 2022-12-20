62f79d762c646c07c06bbf9a54d0c74c06ae3554
Bullets-jyr <jyr@bullets.co.kr>
2022-12-21 08:49:46 +0900

    02. 함수

---
+++ app/src/main/java/kr/co/bullets/kotlin/Example2.kt
@@ -0,0 +1,23 @@
+package kr.co.bullets.kotlin
+
+fun main() {
+    val result = test(1, c = 5)
+    test2(name = "류지영", nickname = "지영", id = "지영님")
+    println(result)
+    println(time1(1, 3))
+    println(time2(1, 3))
+}
+
+// 2. 함수
+fun test(a: Int, b: Int = 3, c: Int = 4): Int {
+    println(a + b)
+    return a + b
+}
+
+fun test2(name: String, nickname: String, id: String) = println(name + nickname + id)
+
+fun time1(a: Int, b: Int): Int {
+    return a * b
+}
+
+fun time2(a: Int, b: Int) = a * b
